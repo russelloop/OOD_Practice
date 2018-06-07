3253fe9bfd2b6564765ad2d930ebd25fcc7caf39
root <reasonsample@outlook.com>
2018-06-07 22:37:29 +0800

    update polygon

--- Polygon/main.c
+++ Polygon/main.c
@@ -1,120 +1,88 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX_POINT 20
-int chain_value[MAX_POINT][MAX_POINT][2];  //chain[i][j][0]表示从v[i]节点开始，边长为j的链的最大值
-//chain[i][j][1]表示从v[i]节点开始，边长为j的链的最小值
-int n,*v;
-char *op;
+#define num 50
 
-int get_max_sum();
-void chain_max(int start, int len, int k);
-void get_max_min(int a, int b, int c, int d, int start, int index, int len);
+int m[num][num][2],s[num][num];
 
-int main()
+
+
+/*获得将第i条边断开后最后一个运算符在i+s处进行得到的最大值;再循环比较不同的s，取出最大值，最后比较不同的i，取出最大值，便为要求的最大值；m[i][j][0]里存储从顶点i开始，长度为j的链p[i][j]合并的最小值，m[i][j][1]存储链*/
+int  getmax(int n,int v[],char op[])
 {
-    int i;
-    char ch;
-
-    scanf("%d",&n);
-    v = (int *)malloc(n*sizeof(int));
-    op = (char *)malloc(n*sizeof(char));
-
-    ch = getchar();                    //数据输入
-    for (i = 0; i < n; i++)
-    {
-        scanf("%c",&op[i]);
-        ch = getchar();
-        scanf("%d",&v[i]);
-        ch = getchar();
-    }
-
-    printf("MAX:%d\n",get_max_sum());
-
-    return 0;
-}//main
-
-
-int get_max_sum()
-{
-    int i,k,start,max,tmp,len;
-
-    for (i = 0; i < n; i++)
-    {
-        chain_value[i][0][0] = v[i];        //链长为0，即单个点的值
-        chain_value[i][0][1] = v[i];
-    }
-
-    for (len = 1; len < n; len++)  //控制链的长度，
-    {
-        for (start = 0; start < n ; start++)        //起点start配合链长，就可以得出除去某条边的效果
+    int i1,j1,k1,s1,x;
+    int temp_ij_min[num],temp_ij_max[num],temp_max;
+    int e[4];
+    for(i1=1;i1<=n;i1++)
+        m[i1][1][0]=m[i1][1][1]=v[i1];
+    for(j1=2;j1<=n;j1++)
+        for(i1=1;i1<=n;i1++)
         {
-            chain_value[start][len][0] = -9999;
-            chain_value[start][len][1] = 9999;
-            for (k = 0; k < len; k++)
+            for(s1=1;s1<=j1-1;s1++)
             {
-                chain_max(start,len,k);
+                x=(i1+s1-1)%n+1;
+                if(op[(i1+s1)%n]=='+')
+                {
+                    temp_ij_min[s1]=m[i1][s1][0]+m[x][j1-s1][0];
+                    temp_ij_max[s1]=m[i1][s1][1]+m[x][j1-s1][1];
+                }
+                else
+                {
+                    e[0]=(m[i1][s1][0])*(m[x][j1-s1][0]);
+                    e[1]=(m[i1][s1][0])*(m[x][j1-s1][1]);
+                    e[2]=(m[i1][s1][1])*(m[x][j1-s1][0]);
+                    e[3]=(m[i1][s1][1])*(m[x][j1-s1][1]);
+                    temp_ij_min[s1]=e[0];
+                    temp_ij_max[s1]=e[0];
+                    for(k1=1;k1<4;k1++)
+                    {
+                        if(temp_ij_min[s1]>e[k1])
+                            temp_ij_min[s1]=e[k1];
+                        if(temp_ij_max[s1]<e[k1])
+                            temp_ij_max[s1]=e[k1];
+                    }
+                }
+            }
+            m[i1][j1][0]=temp_ij_min[1];m[i1][j1][1]=temp_ij_max[1];
+            for(k1=2;k1<=j1-1;k1++)
+            {
+                if(m[i1][j1][0]>temp_ij_min[k1])
+                    m[i1][j1][0]=temp_ij_min[k1];
+                if(m[i1][j1][1]<temp_ij_max[k1])
+                {
+                    m[i1][j1][1]=temp_ij_max[k1];
+                    s[i1][j1]=s1;
+                }
             }
         }
-    }
-
-    max = chain_value[0][n-1][0];            //最终的最大值
-    for (start = 1; start < n; start++)
+    temp_max=m[1][n][1];
+    for(i1=2;i1<=n;i1++)
     {
-        tmp =  chain_value[start][n-1][0];
-        max = max > tmp ? max : tmp;
+        if(temp_max<m[i1][n][1]) temp_max=m[i1][n][1];
     }
-    return max;
+    return temp_max;
 }
 
-void chain_max( int start, int len, int k)
+
+/*主程序*/
+
+int main( )
 {
-    int index,a,b,c,d;  //a<= m1 <= b  c<= m2 <=d
+    int n,max;
+    int i;
+    int v[50];
+    char op[50];
 
-    index = (start+k+1)%n;  //表示符号(op)或者符号后面的操作数的下标
+    printf("input n:");
+    scanf("%d",&n);
+    getchar();
+    for(i=1;i<=n;i++)
+    {
+        scanf("%c %d",&op[i],&v[i]);
+        getchar();
+    }
+    max=getmax(n,v,op);
+    printf("the max is:%d",max);
+    getchar();
 
-    a = chain_value[start][k][1];
-    b = chain_value[start][k][0];
-    c = chain_value[index][len-k-1][1];
-    d = chain_value[index][len-k-1][0];
-
-    get_max_min(a,b,c,d,start,index,len);
 
 }
-
-void get_max_min(int a, int b, int c, int d, int start, int index, int len)
-{
-    int max_max,min_min,max_min,min_max;
-    int max,min;
-
-    max = chain_value[start][len][0];
-    min = chain_value[start][len][1];
-
-    if ('+' == op[index])
-    {
-        max = max > (b + d) ? max : (b + d);
-        min = min < (a + c) ? min : (a + c);
-    }
-    else if ('x' == op[index])
-    {
-        max_max = b * d;
-        min_min = a * c;
-        max_min = b * c;
-        min_max = a * d;
-
-        max = max > max_max ? max :max_max;
-        max = max > min_min ? max : min_min;
-        max = max > max_min ? max : max_min;
-        max = max > min_max ? max : min_max;
-
-        min = min < max_max ? min :max_max;
-        min = min < min_min ? min : min_min;
-        min = min < max_min ? min : max_min;
-        min = min < min_max ? min : min_max;
-    }
-
-    chain_value[start][len][0] = max;
-    chain_value[start][len][1] = min;
-}
-
-//2013/9/14 10:13
