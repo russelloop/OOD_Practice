79bbc2921da09d812cd9a197cd3120487ac89f3a
王雨阳 <reasonsample@outlook.com>
2018-10-29 23:25:06 +0800

    add ImageSalon

---
+++ ImageSalon/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" Inherits="ImageSalon.Default" %>
+<!DOCTYPE html>
+<html>
+    <head runat="server">
+	    <title>Default</title>
+    </head>
+    <body>
+	    <form id="form1" runat="server">
+                <asp:image runat="server" ID="Image" Width="200"></asp:image>
+                <br/>
+                <asp:Button ID="LastPicButton" runat="server" Text="Last" 
+            onclick="LastPicButton_Click" />&nbsp;&nbsp;&nbsp;&nbsp; 
+                <asp:Button ID="NextPicButton" runat="server" Text="Next" 
+            onclick="NextPicButton_Click" />
+                <asp:HiddenField ID="pictureIndex" Value="0" runat="server" />
+	    </form>
+    </body>
+</html>
---
+++ ImageSalon/Default.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+namespace ImageSalon{
+    public partial class Default : System.Web.UI.Page{
+
+        protected void Page_Load(object sender, EventArgs e){
+            if (!IsPostBack){
+                showImage(Int32.Parse(pictureIndex.Value));
+            }
+        }
+        protected string[] getImages(){
+            string[] path = Directory.GetFiles(Request.PhysicalApplicationPath + "picture");
+            return path;
+        }
+        protected void showImage(int i){
+            string[] path = getImages();
+            Image.ImageUrl = path[i].Substring(path[i].LastIndexOf(@"\") + 1).Replace("/Users/dashwood/Projects/ASP.NET/ImageSalon", "~");
+            Image.Visible = true;
+            pictureIndex.Value = i.ToString();
+        }
+
+        protected void LastPicButton_Click(object sender, EventArgs e){
+            if (Int32.Parse(pictureIndex.Value) == 0)
+                return;
+            else{
+                int i = Int32.Parse(pictureIndex.Value);
+                i--;
+                pictureIndex.Value = i.ToString();
+            }
+            showImage(Int32.Parse(pictureIndex.Value));
+        }
+
+        protected void NextPicButton_Click(object sender, EventArgs e){
+            if (Int32.Parse(pictureIndex.Value) == getImages().Length - 1){
+                return;
+            }
+            else{
+                int i = Int32.Parse(pictureIndex.Value);
+                i++;
+                pictureIndex.Value = i.ToString();
+            }
+            showImage(Int32.Parse(pictureIndex.Value));
+        }
+    }
+}
